70154a1d99cd87ea6634951f6676f6b45319ef57
Thomas <tgusewelle@gkwmedia.com>
2024-01-24 11:53:03 -0600

    add in keep alive

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import "dotenv/config";
+import WebSocket, { WebSocketServer } from "ws";
 
-console.log("THis is the port: ", process.env.PORT)
-const wss = new WebSocketServer({port: Number(process.env.PORT)});
+const wss = new WebSocketServer({ port: Number(process.env.PORT) });
 
-let pastorData = { type: "NONE", date: new Date() };
+function heartbeat() {
+  console.log("ping recieved");
+  this.isAlive = true;
+}
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+let pastorData = { type: "NONE", time: new Date() };
 
-  ws.on('message', function message(data, isBinary) {
+wss.on("connection", function connection(ws) {
+  ws.isAlive = true;
+  ws.on("error", console.error);
+  ws.on("pong", heartbeat);
+
+  ws.on("message", function message(data, isBinary) {
     const Jdata = JSON.parse(data);
-    pastorData = {type: Jdata.type, time: new Date(Jdata.time)}
+    pastorData = { type: Jdata.type, time: new Date(Jdata.time) };
     wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
@@ -19,5 +26,18 @@ wss.on('connection', function connection(ws) {
   });
 
   // Send the initial data to the user
-    ws.send(JSON.stringify(pastorData))
+  ws.send(JSON.stringify(pastorData));
+});
+
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 30000);
+
+wss.on("close", function close() {
+  clearInterval(interval);
 });
